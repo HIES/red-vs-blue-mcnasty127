1efe1881d3b3c4f8b1a6906e7f95db28c0374100
mcnasty127 <31041811+mcnasty127@users.noreply.github.com>
2017-12-13 13:56:35 -0500

    WorkingEmptyMap

--- EmptyMap.java
+++ EmptyMap.java
@@ -2,14 +2,13 @@ import java.lang.Exception;
 import java.io.File;
 import java.util.Scanner;
 import static java.lang.System.*;
-
 public class EmptyMap
 {
     public static void visualize(String region) throws Exception
     {
         String fileName = region;
         String extension = ".txt";
-        File inputFile = new File(fileName+extension);
+        File inputFile = new File("input/"+fileName+extension);
         Scanner inputObject = new Scanner(inputFile);
         double xmin = inputObject.nextDouble();
         double ymin = inputObject.nextDouble();
@@ -20,8 +19,11 @@ public class EmptyMap
         int n = inputObject.nextInt();
         inputObject.nextLine();
         StdDraw.setCanvasSize((((int)xmax-(int)xmin)*512)/((int)ymax-(int)ymin),512);
-        StdDraw.setScale(xmax-xmin,ymax-ymin);
+        StdDraw.setXscale(xmin,xmax);
+        StdDraw.setYscale(ymin,ymax);
         StdDraw.setPenColor(0,0,0);
+        StdDraw.enableDoubleBuffering();
+        StdDraw.show();
         for(int i = 0; i < n; i++){
             for(int m = 0; m < 3; m++){
                 inputObject.nextLine();
@@ -36,7 +38,6 @@ public class EmptyMap
             }
             StdDraw.polygon(xs,ys);
         }
-        StdDraw.enableDoubleBuffering();
         StdDraw.show();
         inputObject.close();
     }
